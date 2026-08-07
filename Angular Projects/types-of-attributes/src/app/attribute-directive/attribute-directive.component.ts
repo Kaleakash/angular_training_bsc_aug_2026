d4a5f3fb34fe4b022e2da1253b2a7a5f60c56f11
Akash <akash300383@gmail.com>
2026-08-07 16:57:24 +0530

    done

--- Angular Projects/types-of-attributes/src/app/attribute-directive/attribute-directive.component.ts
+++ Angular Projects/types-of-attributes/src/app/attribute-directive/attribute-directive.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-attribute-directive',
   templateUrl: './attribute-directive.component.html',
   styleUrls: ['./attribute-directive.component.css']
+
 })
 export class AttributeDirectiveComponent {
+styleValue= {'color': 'blue', 'font-size': '18px'};
+f1:boolean = true;
+f2:boolean = false;
+
+fun1(): void {
+  this.f1 = !this.f1;
+  this.f2 = !this.f2;
+}
+fun2() : void {
+this.f1 = !this.f1;
+this.f2 = !this.f2;
+}
 
 }
